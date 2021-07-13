5e1c83dcf8eefb20c4e613a99c1506e618024ee1
Harshit-svmx <harshit.srivastava@servicemax.com>
2021-07-13 20:28:38 +0530

    Calculate - Int --> char

--- main.cpp
+++ main.cpp
@@ -8,16 +8,12 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    int n=5;
-    int value=1;
-    for(int i = 1; i<=n;i++){
-        for(int j = 1; j<=i ;j++){
-            std::cout<<value;
-            value = value +1;
-        }
-        std::cout<<"\n";
-    }
-    
+    int x =65;
+    char ch;
+    ch=x;
+    std::cout<<"\n";
+    std::cout<<ch;
+    std::cout<<"\n";
     
     return 0;
 }
